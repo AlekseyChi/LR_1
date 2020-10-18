fe73f593a30b9822ae27b59e348ac5ea27377f20
AlekseyChi <chiganov.aleksey@mail.ru>
2020-10-18 20:22:53 +0300

    final commit

--- pipeline/pipeline.cpp
+++ pipeline/pipeline.cpp
@@ -1,6 +1,7 @@
 ﻿// pipeline.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -18,26 +19,43 @@ struct compressor_station                           //компрессорная
     string name;                                    //имя
     unsigned int manufactory;                       //колич. цехов
     unsigned int manufactory_w;                     //колич. рабочих цехов
-    unsigned int efficiency;                        //эффективность
+    float efficiency;                        //эффективность
 };
 
-void check(int &a)                                                      //проверка на число
+void CheckInt(unsigned int &a)                                                      //проверка на целое число(int)
 {
-    int i = 1;
-    do {
+    cin >> a;
+    if (cin.fail()) {
         cin.clear();
-        if (i==2)
-        cin.ignore(1000, '\n');
+        while (cin.get() != '\n');                                      //очищаем введенные значения
+        cout << "Введите в следующий раз целое число!" << endl;
+        system("pause");
+        CheckInt(a);
+    };
+};
 
-        if ( i == 2) {
-            i = 2;
-            cin >> a;
-        }
-        else {
-            cout << "Введите правильное значние - ";
-            cin >> a;
-        }
-    } while (cin.fail());
+void CheckFloat(float& a)                                                 //проверка на число с запятой(float)
+{
+    cin >> a;
+    if (cin.fail()) {
+        cin.clear();
+        while (cin.get() != '\n');                                          //очищаем введенные значения
+        cout << "Введите в следующий раз число!" << endl;
+        system("pause");
+        CheckFloat(a);
+    };
+};
+
+void CheckBool(bool& a)                                                 //проверка на булевское значение(float)
+{
+    cin >> a;
+    if (cin.fail()) {
+        cin.clear();
+        while (cin.get() != '\n');                                          //очищаем введенные значения
+        cout << "Введите в следующий 1 или 0!" << endl;
+        system("pause");
+        CheckBool(a);
+    };
 };
 
 void Menu()                                         //Меню
@@ -56,117 +74,142 @@ void Menu()                                         //Меню
 void PlusPipe(pipe &pipe_i)
 {
     cout << "Введите идентификатор - ";
-    cin >> pipe_i.id;
+    CheckInt(pipe_i.id);
     cout << "Введите длину(м) - ";
-    cin >> pipe_i.size_l;
+    CheckFloat(pipe_i.size_l);
     cout << "Введите диаметр(мм) - ";
-    cin >> pipe_i.size_w;
+    CheckInt(pipe_i.size_w);
     cout << "Введите 1, если труба работает или 0 - если труба не работает - ";
-    cin >> pipe_i.work;
+    CheckBool(pipe_i.work);
 };
 
 void PlusCS(compressor_station &CS_i)
 {
     cout << "Введите идентификатор - ";
-    cin >> CS_i.id;
+    CheckInt(CS_i.id);
     cout << "Введите название - ";
     cin >> CS_i.name;
     cout << "Введите количество цехов - ";
-    cin >> CS_i.manufactory;
+    CheckInt(CS_i.manufactory);
     cout << "Введите количество рабочих цехов - ";
-    cin >> CS_i.manufactory_w;
+    CheckInt(CS_i.manufactory_w);
     cout << "Введите эффективность - ";
-    cin >> CS_i.efficiency;
+    CheckFloat(CS_i.efficiency);
+    if (CS_i.efficiency > 1 || CS_i.efficiency < 0) {
+        cout << "Введите в следующий раз значение от 0 до 1!" << endl;
+        system("Pause");
+        PlusCS(CS_i);
+    }
 };
 
 void Viewer(pipe& pipe_i, compressor_station& CS_i)
 {
-    cout << "Труба:" << endl;
-    cout << "Введите идентификатор - " << pipe_i.id << endl;
-    cout << "Введите длину(м) - " << pipe_i.size_l << endl;
-    cout << "Введите диаметр(мм) - " << pipe_i.size_w << endl;
-    cout << "Введите 1, если труба работает или 0 - если труба не работает - " << pipe_i.work << endl;
+    if (pipe_i.size_w != 0) {
+        cout << "Труба:" << endl;
+        cout << "Идентификатор - " << pipe_i.id << endl;
+        cout << "Длина(м) - " << pipe_i.size_l << endl;
+        cout << "Диаметр(мм) - " << pipe_i.size_w << endl;
+        cout << (pipe_i.work ? "Работает" : "Не работает") << endl << endl;
+    }
+    else {
+        cout << "Нет введенных данных"<<endl<<endl;
+    }
 
+    if (CS_i.id != 0) {
     cout << "Компрессорная станция:" << endl;
-    cout << "Введите идентификатор - " << CS_i.id << endl;
-    cout << "Введите название - " << CS_i.name << endl;
-    cout << "Введите количество цехов - " << CS_i.manufactory << endl;
-    cout << "Введите количество рабочих цехов - " << CS_i.manufactory_w << endl;
-    cout << "Введите эффективность - " << CS_i.efficiency << endl;
-    system("pause");
+    cout << "Идентификатор - " << CS_i.id << endl;
+    cout << "Название - " << CS_i.name << endl;
+    cout << "Количество цехов - " << CS_i.manufactory << endl;
+    cout << "Количество рабочих цехов - " << CS_i.manufactory_w << endl;
+    cout << "Эффективность - " << CS_i.efficiency << endl;
+    }
+    else {
+        cout << "Нет введенных данных" << endl << endl;
+    }
 };
 
-void EditPipe()
+void Keep(pipe& pipe_i, compressor_station& CS_i)
 {
-    cout << " ";
+        ofstream OutFile("вывод.txt", ios::app);
+    if (pipe_i.size_w != 0) {
+        OutFile << "Труба:" << endl;
+        OutFile << "Идентификатор - " << pipe_i.id << endl;
+        OutFile << "Длина(м) - " << pipe_i.size_l << endl;
+        OutFile << "Диаметр(мм) - " << pipe_i.size_w << endl;
+        OutFile << (pipe_i.work ? "Работает" : "Не работает") << endl << endl;
+    }
+    else {
+        OutFile << "Нет введенных данных" << endl << endl;
+    };
+
+    if (CS_i.id != 0) {
+    OutFile << "Компрессорная станция:" << endl;
+    OutFile << "Идентификатор - " << CS_i.id << endl;
+    OutFile << "Название - " << CS_i.name << endl;
+    OutFile << "Количество цехов - " << CS_i.manufactory << endl;
+    OutFile << "Количество рабочих цехов - " << CS_i.manufactory_w << endl;
+    OutFile << "Эффективность - " << CS_i.efficiency << endl;
+    }
+    else {
+        OutFile << "Нет введенных данных" << endl << endl;
+    }
 };
 
-void EditCS()
+void Unload(pipe& pipe_i, compressor_station& CS_i)
 {
-    cout << " ";
-};
-
-void Keep()
-{
-    cout << " ";
-};
-
-void Unload()
-{
-    cout << " ";
-};
-
-void InputArg()                                //работа с меню
-{
-    pipe P;
-    compressor_station CS;
-
-    Menu();
-
-    int a;                                          //значение из меню
-    cout << "Выберите действие - ";
-    check(a);
-
-    switch (a)
-    {
-    case 1:
-        PlusPipe(P);
-        break;
-    case 2:
-        PlusCS(CS);
-        break;
-    case 3:
-        Viewer(P,CS);
-        break;
-    case 4:
-        EditPipe();
-        break;
-    case 5:
-        EditCS();
-        break;
-    case 6:
-        Keep();
-        break;
-    case 7:
-        Unload();
-        break;
-    case 0:
-        exit(0);
-        break;
-    default:
-        cout << "Введите верное число"<<endl;
-        int b;
-        cin >> b;
-        InputArg();
-        break;
+    ifstream InFile("ввод.txt");
+    if (!InFile.is_open())
+        cout << "Файл не может быть открыт!\n";
+    else {
+        InFile >> pipe_i.id >> pipe_i.size_l >> pipe_i.size_w >> pipe_i.work;
+        InFile >> CS_i.id >> CS_i.name >> CS_i.manufactory >> CS_i.manufactory_w >> CS_i.efficiency;
     }
 };
 
 int main()
 {    
+    pipe P;
+    compressor_station CS;
+    P.size_w = 0;
+    CS.id = 0;
+    unsigned int a;                                          //значение из меню
     setlocale(LC_ALL, "Russian");
     while (true) {
-        InputArg();
+        Menu();
+        cout << "Выберите действие - ";
+            CheckInt(a);
+            switch (a)
+            {
+            case 1:
+                PlusPipe(P);
+                break;
+            case 2:
+                PlusCS(CS);
+                break;
+            case 3:
+                Viewer(P, CS);
+                system("pause");
+                break;
+            case 4:
+                PlusPipe(P);
+                break;
+            case 5:
+                PlusCS(CS);
+                break;
+            case 6:
+                Keep(P, CS);
+                break;
+            case 7:
+                Unload(P, CS);
+                break;
+            case 0:
+                exit(0);
+                break;
+            default:
+                cout << "Введите верное число!" << endl;
+                system("pause");
+                break;
+            }
     }
 }
 
